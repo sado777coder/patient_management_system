0945338d91eab37df8c0ac97e0ee6f0b124b0834
sado <sottieamosofori12@gmail.com>
2026-02-16 18:14:06 -0800

    Fix:Amount

--- src/validators/labResult.validator.js
+++ src/validators/labResult.validator.js
@@ -2,10 +2,13 @@ const Joi = require("joi");
 
 const createLabResultValidator = Joi.object({
   visit: Joi.string().required(),
+  patient: Joi.string().required(),
 
   testName: Joi.string().required(),
   result: Joi.string().optional(),
   normalRange: Joi.string().optional(),
+
+  amount: Joi.number().required(),
 
   status: Joi.string()
     .valid("pending", "completed")
