81e782d085aeb0b917d76e027b096dc65ccf715c
sado <sottieamosofori12@gmail.com>
2026-02-24 09:11:17 -0800

    Fix:Gravida

--- src/validators/pregnancy.validator.js
+++ src/validators/pregnancy.validator.js
@@ -1,9 +1,24 @@
 const Joi = require("joi");
 
+const objectId = /^[0-9a-fA-F]{24}$/;
+
 const createPregnancyValidator = Joi.object({
-  patient: Joi.string().required(),
+  patient: Joi.string()
+    .pattern(objectId)
+    .required(),
+
+  gravida: Joi.number()
+    .integer()
+    .min(0)
+    .required(),
+
+  para: Joi.number()
+    .integer()
+    .min(0)
+    .required(),
 
   lastMenstrualPeriod: Joi.date().optional(),
+
   expectedDeliveryDate: Joi.date().optional(),
 
   riskLevel: Joi.string()
